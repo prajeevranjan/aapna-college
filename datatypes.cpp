b97fcfd6513bf9973a0624c3865c6429dbaeb4f8
prajeevranjan <prajeevranjan1680@gmail.com>
2025-08-11 09:56:26 +0530

    added int, char and float datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 20;
+    cout << age << endl;
+    cout << sizeof(age) << endl;
+    char grade = 'a';
+    cout <<grade <<endl;
+    float marks = 97.5;
+    cout << marks <<endl; 
+
+}
