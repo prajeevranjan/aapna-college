1abf177bad7e8734b82cfbbcf84baa2954136cf0
prajeevranjan <prajeevranjan1680@gmail.com>
2025-08-11 09:17:50 +0530

    added age

--- 1st.cpp
+++ 1st.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main(){
     string name = "Rajeev";
     string fullname = "Rajeev Ranjan Pandey";
+    int age = 20;
+    cout << age << endl;
     cout<<"Rajeev \n Ranjan \n Pandey" << endl;
     return 0;
 }
