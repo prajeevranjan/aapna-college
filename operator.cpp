7c543bbce49c3411f301fabdc519479cbd466d88
prajeevranjan <prajeevranjan1680@gmail.com>
2025-08-14 21:12:32 +0530

    arithematics operator done

---
+++ operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 100, b = 5;
+    cout <<a + b << endl;
+    cout <<a - b << endl;
+    cout <<a * b << endl;
+    cout <<a / b << endl;
+    cout <<a % b << endl;
+
+    return 0;
+}
