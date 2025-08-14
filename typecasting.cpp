bd963a7565d546542dd25a8d29a48e589a33b8aa
prajeevranjan <prajeevranjan1680@gmail.com>
2025-08-14 20:55:28 +0530

    typecasting completed

--- typecasting.cpp
+++ typecasting.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
+    char grade = 'a';
+    int value = grade;
+    cout << value << endl;
     
+    double price = 99.99;
+    int NewPrice =(int)price;
+    cout << NewPrice << endl;
+
     return 0;
 }
